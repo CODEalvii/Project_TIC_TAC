0f3f8d04e478d358f07c0ba65a22d76d044a1f46
M.Waqas Alvi <waqashadayat144@gmail.com>
2025-06-16 08:51:29 +0500

    Update Tictactoe.cpp

--- Project2/Tictactoe.cpp
+++ Project2/Tictactoe.cpp
@@ -25,14 +25,14 @@ void drawBoard() {
     }
 }
 
-int boardFull() {
+
 int boardFull() {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             if (board[i][j] != 'X' && board[i][j] != 'O') {
                 return 0;
             }
-        }
+    
     }
     return 1;
 }
